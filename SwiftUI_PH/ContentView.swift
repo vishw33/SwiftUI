867c5851341d4b4607d8e3a9f9f78580852e38ef
Vishwas <vishw33@gmail.com>
2019-07-06 09:46:10 +0530

    Initial Commit

---
+++ SwiftUI_PH/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_PH
+//
+//  Created by vishwas ng on 06/07/19.
+//  Copyright © 2019 vishwas ng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
